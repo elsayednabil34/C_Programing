91551701f6093159dea9eb9d51891682f36a5b93
EL-Sayed Nabil Elsayed <113311297+elsayednabil34@users.noreply.github.com>
2023-07-07 17:38:06 +0300

    Update bit_manipulation.h

--- bit_manipulation/bit_manipulation.h
+++ bit_manipulation/bit_manipulation.h
@@ -1,5 +1,5 @@
-#ifndef _BIT_MANIPULATION_H
-#define _BIT_MANIPULATION_H 
+#ifndef _BIT_MANIPULATION_H_
+#define _BIT_MANIPULATION_H_
 
 int set_bit (int number , int bit) ; 
 int clear_bit (int number , int bit);
