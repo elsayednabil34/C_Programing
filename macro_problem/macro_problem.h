3023695a60a8ce4d6712b35e1ac340eeabf2f87b
EL-Sayed Nabil Elsayed <113311297+elsayednabil34@users.noreply.github.com>
2023-07-07 16:12:00 +0300

    Add files via upload

---
+++ macro_problem/macro_problem.h
@@ -0,0 +1,10 @@
+#ifndef _MACRO_PROBLEM_H
+#define _MACRO_PROBLEM_H
+
+
+#define SQUARE1(number) number*number
+#define SQUARE2(number) (number*number)
+#define SQUARE3(number) (number)*(number)
+#define SQUARE4(number) ((number)*(number))
+
+#endif
---
+++ macro_problem/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "macro_problem.h"
+
+int main (void)
+{
+
+    printf("SQUARE1 = %i \n",SQUARE1(2+1));
+    printf("SQUARE2 = %i \n",SQUARE2(2+1));
+    printf("SQUARE3 = %i \n",SQUARE3(2+1));
+    printf("SQUARE4 = %i \n",SQUARE4(2+1));
+
+
+
+}
